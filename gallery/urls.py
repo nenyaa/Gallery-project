f414cb2bab0e7f66e79a813d093d4b275c9633f9
nenyaa <139075287+nenyaa@users.noreply.github.com>
2024-01-10 13:58:01 +0100

    Refactoring

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "gallery"
 urlpatterns = [
-    path("gallery", views.GalleryList.as_view(), name="list"),
-    path("gallery/<int:pk>", views.GalleryDetail.as_view(), name="details"),
-    path("gallery/<int:pk>/images/<int:picture_pk>", views.PictureDetail.as_view(), name="picture-details")
+    path("", views.GalleryList.as_view(), name="list"),
+    path("<int:pk>", views.GalleryDetail.as_view(), name="details"),
+    path("<int:pk>/images/<int:picture_pk>", views.PictureDetail.as_view(), name="picture-details")
     ]
